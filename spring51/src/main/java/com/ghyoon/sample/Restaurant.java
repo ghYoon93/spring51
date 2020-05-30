0e455e03cc2eaf2eeb5d0069e58848fe65ed2b9b
ghYoon93 <gh.yoon93@gmail.com>
2020-05-30 21:04:04 +0900

    Create spring web project

---
+++ spring51/src/main/java/com/ghyoon/sample/Restaurant.java
@@ -0,0 +1,13 @@
+package com.ghyoon.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+public class Restaurant {
+	@Autowired
+    private Chef chef;
+}
